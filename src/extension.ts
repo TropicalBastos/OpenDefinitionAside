f431239b0ed24d105818158f46a8542bc7b53bbe
Ian Bastos <ian-bastos@live.com>
2019-06-02 11:59:45 -0300

    added key bindings

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.addOpenAsideToContextMenu', async () => {
-		await vscode.commands.executeCommand('editor.action.revealDefinitionAside', { openToSide: true });
+		await vscode.commands.executeCommand('editor.action.revealDefinitionAside', 
+		{ 
+			openToSide: true 
+		});
 	});
 
 	context.subscriptions.push(disposable);
